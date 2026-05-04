94de87c8919eda2e53e440ce73cb3bf0adadfeab
Elen216 <anjelina007@ukr.net>
2026-05-04 21:09:18 +0300

    Update d.z.2.2New.py

--- d.z.2.2.py
+++ d.z.2.2.py
@@ -1,7 +1,7 @@
-nums = input("Введіть три числа через кому: ")
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+num3 = float(input("Введіть третє число: "))
 
-a, b, c = map(float, nums.split(","))
-
-average = (a + b + c) / 3
+average = (num1 + num2 + num3) / 3
 
 print("Середнє:", average)
