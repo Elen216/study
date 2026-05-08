e6b80b3fc36cce28d9e61e9f570e529c010a0b9d
Elen216 <anjelina007@ukr.net>
2026-05-08 14:10:13 +0300

    Implement basic calculator functionality

---
+++ d.z.3.1.py
@@ -0,0 +1,20 @@
+while True:
+    num1 = float(input("Введіть перше число: "))
+    num2 = float(input("Введіть друге число: "))
+
+    znak = input("Введіть знак (+, -, *, /): ")
+
+    if znak == "+":
+        print("Результат:", num1 + num2)
+
+    elif znak == "-":
+        print("Результат:", num1 - num2)
+
+    elif znak == "*":
+        print("Результат:", num1 * num2)
+
+    elif znak == "/":
+        if num2 != 0:
+            print("Результат:", num1 / num2)
+        else:
+            print("На нуль ділити не можна")
