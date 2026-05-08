686506db07d51f900233efb0aed5ee1b5a765aa7
Elen216 <anjelina007@ukr.net>
2026-05-08 13:55:17 +0300

    Refactor calculator to run in a loop

--- d.z.5.2.py
+++ d.z.5.2.py
@@ -1,21 +1,29 @@
-num1 = float(input("Введіть перше число: "))
-num2 = float(input("Введіть друге число: "))
-znak = input("Введіть знак (+, -, *, /): ")
-if znak == "+":
-print("Результат:", num1 + num2)
-elif znak == "-":
-print("Результат:", num1 - num2)
-elif znak == "*":
-print("Результат:", num1 * num2)
-elif znak == "/":
-if num2 != 0:
-print("Результат:", num1 / num2)
-else:
-print("На нуль ділити не можна")
-else:
-print("Невірний знак")
-answer = input("Продовжити? (y/n): ")
-if answer != "y":
-print("Калькулятор завершив роботу")
-break
+while True:
+    num1 = float(input("Введіть перше число: "))
+    num2 = float(input("Введіть друге число: "))
 
+    znak = input("Введіть знак (+, -, *, /): ")
+
+    if znak == "+":
+        print("Результат:", num1 + num2)
+
+    elif znak == "-":
+        print("Результат:", num1 - num2)
+
+    elif znak == "*":
+        print("Результат:", num1 * num2)
+
+    elif znak == "/":
+        if num2 != 0:
+            print("Результат:", num1 / num2)
+        else:
+            print("На нуль ділити не можна")
+
+    else:
+        print("Невірний знак")
+
+    answer = input("Продовжити? (y/n): ")
+
+    if answer != "y":
+        print("Калькулятор завершив роботу")
+        break
